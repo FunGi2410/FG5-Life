896149386b198a62f76b1241ccffb1a88fc54336
FunGi2410 <nguyennhanit2410@gmail.com>
2023-11-02 22:42:13 +0700

    state player

--- FG5 Life/Assets/_Scripts/Player.cs
+++ FG5 Life/Assets/_Scripts/Player.cs
@@ -5,16 +5,70 @@ using UnityEngine;
 [RequireComponent(typeof(RequirementPlayer))]
 public class Player : MonoBehaviour
 {
+    // singleton
+    public static Player Instance { get; private set; }
+    void Awake()
+    {
+        Instance = this;
+    }
+
     private ReqPlayerState curState;
-    private RequirementPlayer reqPlayer;
+    public ReqPlayerState CurState
+    {
+        get { return this.curState; }
+        set { this.curState = value; }
+    }
+    //private RequirementPlayer reqPlayer;
+
+    private LivingState eatState;
+    private LivingState toiletState;
+    private LivingState sleepState;
 
     private void Start()
     {
-        this.reqPlayer = GetComponent<RequirementPlayer>();
+        /*this.reqPlayer = GetComponent<RequirementPlayer>();
 
         // test
         this.reqPlayer.CreateState();
         this.curState = this.reqPlayer.GetState();
-        print(this.curState);
+        print(this.curState);*/
+
+        // test with init solid valid
+        // max percent state follow to level player
+        // set after that
+        this.eatState = new LivingState(100, ReqPlayerState.eat);
+        this.toiletState = new LivingState(150, ReqPlayerState.toilet);
+        this.sleepState = new LivingState(200, ReqPlayerState.sleep);
+    }
+
+    public void Eatting()
+    {
+        // test with solid valid with 3
+        this.eatState.Percent += 3;
+        print(this.eatState.Percent);
+    }
+}
+
+public struct LivingState
+{
+    private int percent;
+    public int Percent
+    {
+        get { return percent; }
+        set { percent = value; }
+    }
+
+    private ReqPlayerState nameState;
+    public ReqPlayerState NameState
+    {
+        get { return nameState; }
+        set { nameState = value; }
+    }
+
+
+    public LivingState(int percent, ReqPlayerState nameState)
+    {
+        this.percent = percent;
+        this.nameState = nameState;
     }
 }
--- FG5 Life/Assets/_Scripts/RequirementPlayer.cs
+++ FG5 Life/Assets/_Scripts/RequirementPlayer.cs
@@ -22,6 +22,6 @@ public class RequirementPlayer : MonoBehaviour
 public enum ReqPlayerState
 {
     eat,
-    drink,
-    takeShower
+    sleep,
+    toilet
 }
